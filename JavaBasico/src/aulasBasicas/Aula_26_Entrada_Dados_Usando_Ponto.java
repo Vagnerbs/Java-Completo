41006b0b6ed430465b75505922ee23a548b74abc
Vagnerbs <vagnerb1987@gmail.com>
2025-11-20 18:58:41 -0300

    Aula 26 - Entrada de dados usando ponto no lugar da virgula

---
+++ JavaBasico/src/aulasBasicas/Aula_26_Entrada_Dados_Usando_Ponto.java
@@ -0,0 +1,25 @@
+package aulasBasicas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula_26_Entrada_Dados_Usando_Ponto {
+
+	public static void main(String[] args) {
+		
+		//Locale para podermos usar ponto no lugar da virgula
+		Locale.setDefault(Locale.US);
+		
+		//Scanner para entrada de dados que o usuario digitou
+		Scanner sc = new Scanner(System.in);
+		
+		double x;
+		x = sc.nextDouble();
+		
+		System.out.println("Voce digitou: " + x);
+		
+		sc.close();
+
+	}
+
+}
