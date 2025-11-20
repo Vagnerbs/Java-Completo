3de62c76e26668f3d9bfc6f0d930fb45658b15f4
Vagnerbs <vagnerb1987@gmail.com>
2025-11-20 18:54:27 -0300

    Aula 26 - Entrada de Dados

---
+++ JavaBasico/src/aulasBasicas/Aula_26_Entrada_De_Dados.java
@@ -0,0 +1,21 @@
+package aulasBasicas;
+
+import java.util.Scanner;
+
+public class Aula_26_Entrada_De_Dados {
+
+	public static void main(String[] args) {
+		
+		//Scanner para entrada de dados que o usuario digitou
+				Scanner sc = new Scanner(System.in);
+				
+				String x;
+				x = sc.next();
+				
+				System.out.println("Voce digitou: " + x);
+				
+				sc.close();
+
+	}
+
+}
