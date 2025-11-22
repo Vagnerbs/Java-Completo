59404c1e842c6775ebf7217dadb98bf276e0625f
Vagnerbs <vagnerb1987@gmail.com>
2025-11-22 19:30:22 -0300

    Aula 30 - Calcular e mostrar a diferença do produto

---
+++ JavaBasico/src/aulasBasicas/Aula_030_Exercicio_003.java
@@ -0,0 +1,23 @@
+package aulasBasicas;
+
+import java.util.Scanner;
+
+public class Aula_030_Exercicio_003 {
+
+	public static void main(String[] args) {
+		/*Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+		de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).*/
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Entrada: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+
+		System.out.println("Diferenca = " + ((a * b) - c * d));
+		sc.close();
+	}
+
+}
