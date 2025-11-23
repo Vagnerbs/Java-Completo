a10644066f297201800eb3398729981f46636e9d
Vagnerbs <vagnerb1987@gmail.com>
2025-11-22 21:12:42 -0300

    Entrada de dados

--- JavaBasico/src/secao_04_EstruturaSequencial/Aula_026_Entrada_De_Dados.java
+++ JavaBasico/src/secao_04_EstruturaSequencial/Aula_026_Entrada_De_Dados.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class Aula_026_Entrada_De_Dados {
 
 	public static void main(String[] args) {
-		
-		//Scanner para entrada de dados que o usuario digitou
-				Scanner sc = new Scanner(System.in);
-				
-				String x;
-				x = sc.next();
-				
-				System.out.println("Voce digitou: " + x);
-				
-				sc.close();
+
+		// Scanner para entrada de dados que o usuario digitou
+		Scanner sc = new Scanner(System.in);
+
+		String x;
+		x = sc.next();
+
+		System.out.println("Voce digitou: " + x);
+
+		sc.close();
 	}
 
 }
