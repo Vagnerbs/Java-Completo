ffa9a68e2fce966547c1b0d899afc97491fca397
Vagnerbs <vagnerb1987@gmail.com>
2025-11-22 21:22:45 -0300

    Calcular o valor da área de um circulo

--- JavaBasico/src/secao_04_EstruturaSequencial/Aula_030_Exercicio_002.java
+++ JavaBasico/src/secao_04_EstruturaSequencial/Aula_030_Exercicio_002.java
@@ -7,6 +7,12 @@ public class Aula_030_Exercicio_002 {
 
 	public static void main(String[] args) {
 		
+		/*
+		 * Faça um programa para ler o valor do raio de um círculo, e depois mostrar o
+		 * valor da área deste círculo com quatro casas decimais conforme exemplos.
+		 * Fórmula da área: area = π . raio2 Considere o valor de π = 3.14159
+		 */
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
