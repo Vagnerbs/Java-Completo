cba1ed147338255858ffa52981d009f2f6c1abc0
Vagnerbs <vagnerb1987@gmail.com>
2025-11-22 21:20:40 -0300

    Soma de dois numeros

--- JavaBasico/src/secao_04_EstruturaSequencial/Aula_030_Exercicio_001.java
+++ JavaBasico/src/secao_04_EstruturaSequencial/Aula_030_Exercicio_001.java
@@ -5,16 +5,21 @@ import java.util.Scanner;
 public class Aula_030_Exercicio_001 {
 
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
 		
+		/*
+		 * Faça um programa para ler dois valores inteiros, e depois mostrar na tela a
+		 * soma desses números com uma mensagem explicativa, conforme exemplos.
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println("Entrada: ");
 		int n1 = sc.nextInt();
 		sc.nextLine();
 		int n2 = sc.nextInt();
-		
+
 		System.out.println("SOMA = " + (n1 + n2));
-		
+
 		sc.close();
 	}
 
